a4f2e407d560e9248caa4174fa039e5b0bb962f6
Alex Seifert <alex@alexseifert.com>
2024-10-03 08:50:24 +0200

    Add more headers

--- templates/header.php
+++ templates/header.php
@@ -8,6 +8,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="Content-Language" content="en">
+    <meta name="author" content="Alex Seifert">
+    <meta name="copyright" content="Copyright (c) Alex Seifert">
+    <meta name="theme-color" content="#fbbc05">
+    <meta name="color-scheme" content="light dark">
     <meta name="description" content="<?=$description?>">
     <title><?= $title ?></title>
 </head>
