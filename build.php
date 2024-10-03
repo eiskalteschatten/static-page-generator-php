5d2cd23d9b9fa02ac7cf079848dbbf0ceb26a416
Alex <alex@alexseifert.com>
2024-10-03 14:25:37 +0200

    Move the code for compling pages into its own function

--- build.php
+++ build.php
@@ -5,28 +5,33 @@ $distDir = __DIR__ . DIRECTORY_SEPARATOR . 'public';
 
 // TODO: copy assets to the dist folder and add a unix timestamp to the file name. Also update the getAsset function to use the timestamp.
 
-$iterator = new RecursiveIteratorIterator(
-    new RecursiveDirectoryIterator($rootDir, RecursiveDirectoryIterator::SKIP_DOTS),
-    RecursiveIteratorIterator::SELF_FIRST
-);
+function compilePages() {
+    global $rootDir, $distDir;
 
-foreach ($iterator as $file) {
-    if ($file->isDir()) {
-        $indexPath = $file->getPathname() . DIRECTORY_SEPARATOR . 'index.php';
-        if (file_exists($indexPath)) {
-            ob_start();
-            include_once $indexPath;
-            $output = ob_get_clean();
-            $relativePath = str_replace($rootDir, '', $file->getPathname());
-            $outputFilePath = $distDir . $relativePath . DIRECTORY_SEPARATOR . 'index.html';
+    $iterator = new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator($rootDir, RecursiveDirectoryIterator::SKIP_DOTS),
+        RecursiveIteratorIterator::SELF_FIRST
+    );
 
-            if (!is_dir(dirname($outputFilePath))) {
-                mkdir(dirname($outputFilePath), 0777, true);
+    foreach ($iterator as $file) {
+        if ($file->isDir()) {
+            $indexPath = $file->getPathname() . DIRECTORY_SEPARATOR . 'index.php';
+            if (file_exists($indexPath)) {
+                ob_start();
+                include_once $indexPath;
+                $output = ob_get_clean();
+                $relativePath = str_replace($rootDir, '', $file->getPathname());
+                $outputFilePath = $distDir . $relativePath . DIRECTORY_SEPARATOR . 'index.html';
+
+                if (!is_dir(dirname($outputFilePath))) {
+                    mkdir(dirname($outputFilePath), 0777, true);
+                }
+
+                file_put_contents($outputFilePath, $output);
+                echo $indexPath . ' -> ' . $outputFilePath . PHP_EOL;
             }
-
-            file_put_contents($outputFilePath, $output);
-
-            echo $indexPath . ' -> ' . $outputFilePath . PHP_EOL;
         }
     }
 }
+
+compilePages();
