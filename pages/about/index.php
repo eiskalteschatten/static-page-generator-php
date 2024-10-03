bdfb0a32349c73400f232e131a74129f962e502f
Alex Seifert <alex@alexseifert.com>
2024-10-03 09:07:10 +0200

    Add an about page

---
+++ pages/about/index.php
@@ -0,0 +1,14 @@
+<?php
+    require_once '../../functions.php';
+
+    getHeader([
+        'title' => 'About',
+        // 'description' => 'This is the homepage'
+    ]);
+?>
+
+<div>
+    This is the about page.
+</div>
+
+<?php getFooter(); ?>
