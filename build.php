e0938f0f59fe51a6a072fc99b01f46eb2020e125
Alex Seifert <alex@alexseifert.com>
2024-10-03 09:12:48 +0200

    Start the build script

--- build.php
+++ build.php
@@ -1,3 +1,33 @@
 <?php
 
+$rootDir = __DIR__;
+$distDir = __DIR__ . DIRECTORY_SEPARATOR . 'dist';
+
+$iterator = new RecursiveIteratorIterator(
+    new RecursiveDirectoryIterator($rootDir, RecursiveDirectoryIterator::SKIP_DOTS),
+    RecursiveIteratorIterator::SELF_FIRST
+);
+
+foreach ($iterator as $file) {
+    if ($file->isDir()) {
+        $indexPath = $file->getPathname() . DIRECTORY_SEPARATOR . 'index.php';
+        if (file_exists($indexPath)) {
+            echo "Building " . $indexPath . PHP_EOL;
+
+            ob_start();
+            include $indexPath;
+            $output = ob_get_clean();
+            $relativePath = str_replace($rootDir, '', $file->getPathname());
+            $outputFilePath = $distDir . $relativePath . DIRECTORY_SEPARATOR . 'index.html';
+
+            if (!is_dir(dirname($outputFilePath))) {
+                mkdir(dirname($outputFilePath), 0777, true);
+            }
+
+            file_put_contents($outputFilePath, $output);
+        }
+    }
+}
+
+
 // TODO: copy assets to the dist folder and add a unix timestamp to the file name. Also update the getAsset function to use the timestamp.
